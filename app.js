34b8010bb689c6701ab16542c454ad615b37954a
AvarageCodeEnjoyer <111468739+AvarageCodeEnjoyer@users.noreply.github.com>
2023-02-07 14:13:00 -0700

    fixed class function

--- app.js
+++ app.js
@@ -1,30 +1,36 @@
 const ulList = document.getElementById('ulList')
 const button = document.getElementById('btn')
-const newFriend = new Friends
 let friendsARR = []
 var i = 0
-
-
-button.addEventListener("click", newFriend)
 
 class Friends{
   constructor(name, school, age){
     this.name = name
     this.school = school
     this.age = age
-    addFriends(); {
-      let name = prompt("Whats their name?")
-      let school = prompt("Where do they go to school")
-      let age = prompt("How old are they?")
-    
-      friendsARR[i] = new Friends(name, school, age)
-      addLI(friendsARR[i])
-      console.log(friendsARR[i])
-      i++
-    }
   }
+  addFriends() {
+    let createLI = document.createElement('LI')
+    createLI.innerHTML = `<h1>${this.name}</h1>
+                          School: ${this.school} <br>
+                          Age: ${this.age}`
+    ulList.appendChild(createLI)
+    console.log(friendsARR[i])
+    i++
+    return
+  }  
 }
 
+function addArray(){
+  let Name = document.getElementById('name').value
+  let School = document.getElementById('school').value
+  let Age = document.getElementById('age').value
+  friendsARR[i] = new Friends(Name, School, Age)
+  return friendsARR[i].addFriends()
+}
+
+
+button.addEventListener("click", addArray)
 
 
 /* function addFriends(){
@@ -40,13 +46,13 @@ class Friends{
 
 
 
-function addLI(item){
+/* function addLI(item){
   const createLI = document.createElement('LI')
   createLI.innerHTML = `<h1>${item.name}</h1>
                         School: ${item.school} <br>
                         Age: ${item.age}`
   ulList.appendChild(createLI)
-}
+} */
 
 
 
